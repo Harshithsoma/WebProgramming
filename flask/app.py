7b24003aa1f9f0dabbf48771b863ce0aeaffbe14
Harshith <78254938+Harshithsoma@users.noreply.github.com>
2024-02-23 15:01:21 -0500

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -12,7 +12,7 @@ def get_hello():
     me = "Harshith"
     return f"<p>Hello, {me} from the World!</p>"
 
-@app.route("/bye")
+@app.route("/goodbye")
 def get_goodbye():
     me = "Harshith"
     return f"<p>Goodbye, {me} from the World! Come back soon!</p>"
